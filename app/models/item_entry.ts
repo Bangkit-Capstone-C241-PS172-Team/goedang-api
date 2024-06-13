4acd85eb7edebf816f43989ad82f362d625e212d
Rexhark <105967519+Rexhark@users.noreply.github.com>
2024-06-14 00:38:51 +0800

    Update item_entry.ts

--- app/models/item_entry.ts
+++ app/models/item_entry.ts
@@ -5,6 +5,21 @@ export default class ItemLog extends BaseModel {
   @column({ isPrimary: true })
   declare id: string
 
+  @column()
+  declare item_id: number
+
+  @column()
+  declare in_out: boolean
+
+  @column()
+  declare quantity: number
+
+  @column()
+  declare price: number
+
+  @column()
+  declare total: number
+
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
 
