ebcc0a7c53a4b55b9e0ea57cb8280ce810935a51
Rexhark <105967519+Rexhark@users.noreply.github.com>
2024-06-14 11:35:51 +0800

    Update routes.ts

--- start/routes.ts
+++ start/routes.ts
@@ -9,10 +9,14 @@
 
 import router from '@adonisjs/core/services/router'
 // Item
-router.get('/item', '#controllers/items_controller.index')
-router.post('/item', '#controllers/items_controller.store')
-router.get('/items/delete/:id', '#controllers/items_controller.destroy')
+router.get('/items', '#controllers/items_controller.index')
+router.post('/items', '#controllers/items_controller.store')
+router.get('/items/:id', '#controllers/items_controsller.show')
+router.put('/items/:id', '#controllers/items_controller.update')
+router.delete('/items/:id', '#controllers/items_controller.destroy')
 // Item Entries
 router.get('/item_entries', '#controllers/item_entries_controller.index')
 router.post('/item_entries', '#controllers/item_entries_controller.store')
-router.get('/item_entries/delete/:id', '#controllers/item_entries_controller.destroy')
+router.get('/item_entries/:id', '#controllers/item_entries_controller.show')
+router.put('/item_entries/:id', '#controllers/item_entries_controller.update')
+router.delete('/item_entries/:id', '#controllers/item_entries_controller.destroy')
