57d1a853eb5a9d6df941439bea9898ea282f512b
Rexhark <105967519+Rexhark@users.noreply.github.com>
2024-06-15 02:40:48 +0800

    Update routes.ts

--- start/routes.ts
+++ start/routes.ts
@@ -9,19 +9,23 @@
 
 import router from '@adonisjs/core/services/router'
 import { middleware } from './kernel.js'
-// Item
-router.get('/items', '#controllers/items_controller.index')
-router.post('/items', '#controllers/items_controller.store')
-router.get('/items/:id', '#controllers/items_controller.show')
-router.put('/items/:id', '#controllers/items_controller.update')
-router.delete('/items/:id', '#controllers/items_controller.destroy')
 
-// Item Entries
-router.get('/item_entries', '#controllers/item_entries_controller.index')
-router.post('/item_entries', '#controllers/item_entries_controller.store')
-router.get('/item_entries/:id', '#controllers/item_entries_controller.show')
-router.put('/item_entries/:id', '#controllers/item_entries_controller.update')
-router.delete('/item_entries/:id', '#controllers/item_entries_controller.destroy')
+router
+  .group(() => {
+    // Item
+    router.get('/items', '#controllers/items_controller.index')
+    router.post('/items', '#controllers/items_controller.store')
+    router.get('/items/:id', '#controllers/items_controller.show')
+    router.put('/items/:id', '#controllers/items_controller.update')
+    router.delete('/items/:id', '#controllers/items_controller.destroy')
+    // Item Entries
+    router.get('/item_entries', '#controllers/item_entries_controller.index')
+    router.post('/item_entries', '#controllers/item_entries_controller.store')
+    router.get('/item_entries/:id', '#controllers/item_entries_controller.show')
+    router.put('/item_entries/:id', '#controllers/item_entries_controller.update')
+    router.delete('/item_entries/:id', '#controllers/item_entries_controller.destroy')
+  })
+  .use(middleware.ensureLogin())
 
 // Login & Register
 router.post('/register', '#controllers/auth_controller.register').as('auth.register')
