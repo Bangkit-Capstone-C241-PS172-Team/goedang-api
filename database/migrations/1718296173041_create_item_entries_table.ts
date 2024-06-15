3f652cad224bdf742085b0b28993736a4663903f
Rexhark <105967519+Rexhark@users.noreply.github.com>
2024-06-15 13:20:17 +0800

    Update 1718296173041_create_item_entries_table.ts

--- database/migrations/1718296173041_create_item_entries_table.ts
+++ database/migrations/1718296173041_create_item_entries_table.ts
@@ -6,12 +6,12 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.uuid('id').primary().notNullable()
-      table.uuid('item_id').notNullable().unsigned().references('items.id').onDelete('CASCADE')
+      table.uuid('item_id').notNullable().references('items.id').onDelete('CASCADE')
       table.enu('in_out', ['in', 'out']).notNullable()
       table.integer('quantity').notNullable()
       table.integer('price').notNullable()
       table.integer('total').notNullable()
-      table.uuid('user_id').notNullable().unsigned().references('users.id').onDelete('CASCADE')
+      table.uuid('user_id').notNullable().references('users.id').onDelete('CASCADE')
       table.timestamps(true, true)
     })
   }
