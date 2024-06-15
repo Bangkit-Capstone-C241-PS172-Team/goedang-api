67223eb1140d54d64226e15cae2c6273c84aa0df
Rexhark <105967519+Rexhark@users.noreply.github.com>
2024-06-15 13:19:59 +0800

    Update 1718294702969_create_access_tokens_table.ts

--- database/migrations/1718294702969_create_access_tokens_table.ts
+++ database/migrations/1718294702969_create_access_tokens_table.ts
@@ -6,13 +6,7 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.uuid('id').primary().notNullable()
-      table
-        .uuid('tokenable_id')
-        .notNullable()
-        .unsigned()
-        .references('id')
-        .inTable('users')
-        .onDelete('CASCADE')
+      table.uuid('tokenable_id').notNullable().references('id').inTable('users').onDelete('CASCADE')
       table.string('type').notNullable()
       table.string('name').nullable()
       table.string('hash').notNullable()
