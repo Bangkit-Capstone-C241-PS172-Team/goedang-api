2eeb858f745c73a67ce7aa6b107d95059079d2cb
Rexhark <105967519+Rexhark@users.noreply.github.com>
2024-06-15 13:13:08 +0800

    Update 1718296163660_create_items_table.ts

--- database/migrations/1718296163660_create_items_table.ts
+++ database/migrations/1718296163660_create_items_table.ts
@@ -5,11 +5,11 @@ export default class extends BaseSchema {
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id')
+      table.uuid('id').primary()
       table.string('name').notNullable()
       table.integer('quantity').notNullable()
       table.enu('measuring_unit', ['lt', 'kg', 'pcs', 'box', 'roll', 'pack', 'sheet']).notNullable()
-      table.integer('user_id').notNullable().unsigned().references('users.id').onDelete('CASCADE')
+      table.uuid('user_id').notNullable().unsigned().references('users.id').onDelete('CASCADE')
       table.timestamps(true, true)
     })
   }
