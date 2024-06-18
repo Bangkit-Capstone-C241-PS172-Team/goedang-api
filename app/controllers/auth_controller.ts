fb96b7bf42d5c83a603f4aba430164e5572ea069
Rexhark <105967519+Rexhark@users.noreply.github.com>
2024-06-18 17:12:41 +0800

    fixing login and register response

--- app/controllers/auth_controller.ts
+++ app/controllers/auth_controller.ts
@@ -6,13 +6,23 @@ export default class AuthController {
   async register({ request }: HttpContext) {
     const data = await request.validateUsing(registerValidator)
     const user = await User.create(data)
-    return User.accessTokens.create(user)
+    const accessTokens = await User.accessTokens.create(user)
+    const resData = {
+      accessTokens: accessTokens,
+      user: user,
+    }
+    return resData
   }
 
   async login({ request }: HttpContext) {
     const { email, password } = await request.validateUsing(loginValidator)
     const user = await User.verifyCredentials(email, password)
-    return User.accessTokens.create(user)
+    const accessTokens = await User.accessTokens.create(user)
+    const resData = {
+      accessTokens: accessTokens,
+      user: user,
+    }
+    return resData
   }
 
   async logout({ auth }: HttpContext) {
